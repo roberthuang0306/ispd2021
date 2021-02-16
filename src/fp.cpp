13cca0cf11d67b6089b3ca68daa72d830c5967bd
nk930439gj94 <nk930439gj94@gmail.com>
2021-02-16 17:51:36 +0800

    list function needed & partition wip

--- src/fp.cpp
+++ src/fp.cpp
@@ -1,15 +1,16 @@
 #include "fp.h"
 
+int Tile::_Dimension = 0;
+
 FP::FP()
 {
     _Dimension = 0;
     _HeatMap = 0;
-    for(int i = 0; i < D; ++i) _HeatMapSize[i] = 0;
+    _HeatMapSize = 0;
     for(int i = 0; i < 2; ++i) _PESize[i] = 0;
     for(int i = 0; i < 2; ++i) _CostCoefficients[i] = 0.0;
     _S = 0;
-    _Tiles = 0;
-    _n_Tiles = 0;
+    _ComputeRatio = 0.0;
 }
 
 FP::~FP()
@@ -23,12 +24,15 @@ FP::~FP()
         }
         delete [] _HeatMap;
     }
-    if( _Tiles ) {
-        delete [] _Tiles;
+    if( _HeatMapSize ) {
+        delete [] _HeatMapSize;
     }
 }
 
-int FP::readHeatMap(string filename)
+
+// Public Funcs
+
+int FP::readHeatMap(const string& filename)
 {
     ifstream file(filename);
     if(!file.is_open()){
@@ -37,7 +41,7 @@ int FP::readHeatMap(string filename)
     }
     string line;
     int n_line = 0;
-    size_t start, end;
+    size_t start;
     int x = 0, y = 0, z = 0;
     while(getline(file, line)) {
         if(line[0] == '#') continue;
@@ -45,46 +49,40 @@ int FP::readHeatMap(string filename)
         if(n_line == 0) {
             // Dimension
             _Dimension = stoi(line);
+            assert(_Dimension == 2 || _Dimension == 3);
         }
         else if(n_line == 1) {
             // HeatMap size
-            start = 0;
+            _HeatMapSize = new int[_Dimension];
             for(int i = 0; i < _Dimension; ++i) {
-                end = line.find(' ', start);
-                _HeatMapSize[i] = stoi( line.substr(start, end - start) );
-                start = end + 1;
+                _HeatMapSize[i] = stoi( getLineNextToken(line, start, !i) );
             }
             _HeatMap = new double**[_HeatMapSize[0] + 1];
             for(int i = 0; i < _HeatMapSize[0] + 1; ++i) {
                 _HeatMap[i] = new double*[_HeatMapSize[1] + 1];
+                int tmp = (_Dimension == 2 ? 1 : _HeatMapSize[2] + 1);
                 for(int j = 0; j < _HeatMapSize[1] + 1; ++j) {
-                    _HeatMap[i][j] = new double[_HeatMapSize[2] + 1];
+                    _HeatMap[i][j] = new double[tmp];
                 }
             }
         }
         else if(n_line == 2) {
             // PE size
-            start = 0;
             for(int i = 0; i < 2; ++i) {
-                end = line.find(' ', start);
-                _PESize[i] = stoi( line.substr(start, end - start) );
-                start = end + 1;
+                _PESize[i] = stoi( getLineNextToken(line, start, !i) );
             }
         }
         else if(n_line == 3) {
             // Cost parameters
-            start = 0;
             for(int i = 0; i < 2; ++i) {
-                end = line.find(' ', start);
-                _CostCoefficients[i] = stod( line.substr(start, end - start) );
-                start = end + 1;
+                _CostCoefficients[i] = stod( getLineNextToken(line, start, !i) );
             }
         }
         else {
             // HeatMap
             _HeatMap[x][y][z] = stod(line);
-            if( x == _HeatMapSize[0] ) {
-                if( y == _HeatMapSize[1] ) {
+            if(x == _HeatMapSize[0]) {
+                if(y == _HeatMapSize[1]) {
                     ++z;
                     y = 0;
                 }
@@ -102,13 +100,109 @@ int FP::readHeatMap(string filename)
     return 1;
 }
 
-void FP::printHeatMap()
+void FP::Calculate_S()
 {
-    for(int k = 0; k < _HeatMapSize[2] + 1; ++k) {
-        for(int j = 0; j < _HeatMapSize[1] + 1; ++j) {
-            for(int i = 0; i < _HeatMapSize[0] + 1; ++i) {
-                cout << i << " " << j << " " << k << "\t" << _HeatMap[i][j][k] << endl;
+    // TODO
+    _S = 0;
+}
+
+void FP::Init_Partition()
+{
+    assert(_S%10 == 0);
+    Tile::setDimension(_Dimension);
+    _Tiles.clear();
+    int id;
+    int* pos = new int[_Dimension];
+    for(int z = 0; z < (_Dimension == 2 ? 1 : _S * _HeatMapSize[2]); ++z) {
+        if (_Dimension == 3) pos[2] = z;
+        for(int y = 0; y < _S * _HeatMapSize[1]; ++y) {
+            pos[1] = y;
+            for(int x = 0; x < _S * _HeatMapSize[0]; ++x) {
+                pos[0] = x;
+                _Tiles.emplace_back(id++, pos);
             }
         }
     }
+    delete [] pos;
+
+    _Connectivities.clear();
+    int n_sampling_points = _S * _HeatMapSize[0] * _S * _HeatMapSize[1];
+    if(_Dimension == 3) n_sampling_points *= (_S * _HeatMapSize[2]);
+    for(int i = 0; i < n_sampling_points; ++i) _Connectivities.emplace_back();
+    bool left, right, front, back, down, up;
+    id = 0;
+    for(int z = 0; z < (_Dimension == 2 ? 1 : _S * _HeatMapSize[2]); ++z) {
+        down = (z != 0);
+        up = (z != _S * _HeatMapSize[2] - 1);
+        for(int y = 0; y < _S * _HeatMapSize[1]; ++y) {
+            front = (y != 0);
+            back = (y != _S * _HeatMapSize[1] - 1);
+            for(int x = 0; x < _S * _HeatMapSize[0]; ++x) {
+                left = (x != 0);
+                right = (x != _S * _HeatMapSize[0] - 1);
+                if(left) _Connectivities[id].push_back(id - 1);
+                if(right) _Connectivities[id].push_back(id + 1);
+                if(front) _Connectivities[id].push_back(id - _S * _HeatMapSize[0]);
+                if(back) _Connectivities[id].push_back(id + _S * _HeatMapSize[0]);
+                if(_Dimension == 3 && down) _Connectivities[id].push_back(id - _S * _HeatMapSize[0] * _S * _HeatMapSize[1]);
+                if(_Dimension == 3 && up) _Connectivities[id].push_back(id + _S * _HeatMapSize[0] * _S * _HeatMapSize[1]);
+                ++id;
+            }
+        }
+    }
+}
+
+void FP::setComputeRatio(const double& r)
+{
+    _ComputeRatio = r;
+}
+
+void FP::Partition()
+{
+
+}
+
+inline double FP::HeatMap_Resolution(int x, int y, int z)
+{
+    return _HeatMap[x][y][z];
+}
+
+
+// Private Funcs
+
+void FP::printHeatMap()
+{
+    for(int k = 0; k < (_Dimension == 2 ? 1 : _HeatMapSize[2] + 1); ++k) {
+        for(int j = 0; j < _HeatMapSize[1] + 1; ++j) {
+            for(int i = 0; i < _HeatMapSize[0] + 1; ++i) {
+                cout << i << " " << j;
+                if(_Dimension == 3) cout << " " << k;
+                cout << "    " << _HeatMap[i][j][k] << endl;
+            }
+        }
+    }
+}
+
+double FP::Integral_Resolution3()
+{
+    // TODO
+    return 0.0;
+}
+
+double FP::Integral_Resolution(int* origin, int* width)
+{
+    // TODO
+    assert(_S);
+    return 0.0;
+}
+
+
+// Utils
+
+const string& FP::getLineNextToken(const string& line, size_t& start, bool begin)
+{
+    size_t end = line.find(' ', begin ? 0 : start);
+    static string s = line.substr(start, end - start);
+    start = end + 1;
+    return s;
 }
--- src/fp.h
+++ src/fp.h
@@ -2,8 +2,10 @@
 #define FP_H
 
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -11,54 +13,85 @@ class Tile;
 class FP;
 
 // Max Dimension
-#define D 3
+typedef vector< vector<int> > adjacent_list;
 
 
 class Tile
 {
 public:
+    Tile(int Id, int* HeatMapPos) {
+        _Id = Id;
+        _R = 0;
+        _HeatMapPos = new int [_Dimension];
+        for(int i = 0; i < _Dimension; ++i) _HeatMapPos[i] = HeatMapPos[i];
+    }
+
+    ~Tile() {
+        delete [] _HeatMapPos;
+    }
+
+    static void setDimension(int Dimension) {
+        _Dimension = Dimension;
+    }
 
 private:
     int _Id;
-    int _R; // 2^(-r) = resolution
-    int _Size[D];
+    int _R;                 // 2^(-R) = Resolution
 
+    int* _HeatMapPos;       // x10 -> sampling space
     int _PEPos[2];
-    int _HeatMapPos[D];
-};
 
+    static int _Dimension;
+};
 
 class FP
 {
 public:
     FP();
     ~FP();
-    int readHeatMap(string filename);
+    int             readHeatMap(const string& filename);            // Read an input HeatMap
+    void            Calculate_S();                                  // Calculate inverse sampling step
+    void            Init_Partition();                               // Initialize Tiles and Connectivities
+    void            setComputeRatio(const double& r);
+    void            Partition();                                    // Find a legal partition of HeatMap
+    void            Place();                                        // Place all the tiles and adpaters on PE array
+
+    inline double   HeatMap_Resolution(int x, int y, int z=0);
 
     // Debug
-    void printHeatMap();
+    void            printHeatMap();
+    
 private:
     // Input
-    int         _Dimension;
-    double***   _HeatMap;
-    int         _HeatMapSize[D];
-    int         _PESize[2];
+    int             _Dimension;                                     // Dimension of the HeatMap
+    double***       _HeatMap;
+    int*            _HeatMapSize;
+    int             _PESize[2];
 
     // Cost parameters
-    double      _CostCoefficients[2];
+    double          _CostCoefficients[2];                           // Alpha and Beta
     
     // Solution
-    int         _S; // inverse sampling step
-    Tile*       _Tiles;
-    int         _n_Tiles;
+    int             _S;                                             // x10 -> inverse sampling step
+    vector<Tile>    _Tiles;
+
+    adjacent_list   _Connectivities;
 
     // todo
-    // Connections
     // Adapters
 
 
     // Parameters
-    double      _ComputeRatio;
+    double          _ComputeRatio;
+
+
+    // Funcs
+    double          Integral_Resolution3();                         // Integrate Resolution^3 over the entire heatmap
+    double          Integral_Resolution(int* origin, int* width);   // Integrate Resolution over some area in sampling space
+
+
+    // Utils
+    const string&   getLineNextToken(const string& line, size_t& start, bool begin=false);
 };
 
 #endif
